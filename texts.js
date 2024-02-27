1a7007da3ea5f19667567a9ae4823232ed7c573d
Peter Olson <peter.e.c.olson+github@gmail.com>
2024-02-27 16:32:03 -0600

    continue

--- texts.js
+++ texts.js
@@ -1,7 +1,7 @@
 export const texts = [
-    {
-        title: "Schleicher's Fable",
-        text: `
+  {
+    title: "Schleicher's Fable",
+    text: `
         SHEEP nom HORSE nom AND
 
         SHEEP dat IT f sg dat HAIR nos sg nom NOT BE _ira 3 sg sec , THIS f nom HORSE acc pl SEE _pra sec 3 sg .
@@ -9,20 +9,22 @@ export const texts = [
         SHEEP nom HORSE dat pl SPEAK _prr sec 3 sg : \`\` EARTHLING o sg acc LOOK yeti prim 1 sg HORSE acc pl AND DRIVE _irt prim 3 sg , HEART I gen sg UPSET newti middle prim 3 sg ''
         HORSE nom NOW SPEAK _prr sec 3 pl : \`\` HEAR _pra imp 2 sg , SHEEP voc sg ! THIS n acc sg LOOK yeti prim 1 pl , WE dat UPSET newti middle prim 3 sg HEART : EARTHLING o sg , MASTER , SELF SHEEP gen sg HAIR nos sg nom WARM mos acc sg CLOTHE trom acc sg WORK _pra 3 sg sec , SHEEP dat pl NOW HAIR nos sg nom NOT BE _ira prim 3 sg . ''
         THIS n acc sg HEAR _srr part , SHEEP nom FIELD acc sg FLEE _prt sec 3 sg .`,
-        translation: `The Sheep and the Horses
+    translation: `The Sheep and the Horses
 
         A sheep that had no wool saw horses, one of them pulling a heavy wagon, one carrying a big load, and one carrying a man quickly. 
         The sheep said to the horses: "My heart pains me, seeing a man driving horses." 
         The horses said: "Listen, sheep, our hearts pain us when we see this: a man, the master, makes the wool of the sheep into a warm garment for himself. And the sheep has no wool." 
         Having heard this, the sheep fled into the plain`,
-    },
-    {
-        title: "The King and the God",
-        text: `STRAIGHT s nom SKY o_s nom AND
+  },
+  {
+    title: "The King and the God",
+    text: `STRAIGHT s nom SKY o_s nom AND
         
-        STRAIGHT s nom BE _ira 3 sg sec ; THIS m nom NOT- CHILD los m nom .`,
-        translation: `The king and the god
+        STRAIGHT s nom BE _ira 3 sg sec ; THIS m nom NOT- DESCENDANT los m nom .
+        STRAIGHT s nom BIRTH us m acc WANT _irt sec 3 sg .
+        THIS m nom THIS n sg gen POUR tor acc ASK _ira sec 3 sg : \`\` BIRTH us m nom I gen sg '' `,
+    translation: `The king and the god
         
         Once there was a king. He was childless. The king wanted a son. He asked his priest: "May a son be born to me!" The priest said to the king: "Pray to the god Werunos." The king approached the god Werunos to pray now to the god. "Hear me, father Werunos!" The god Werunos came down from heaven. "What do you want?" "I want a son." "Let this be so," said the bright god Werunos. The king's lady bore a son.`,
-    },
+  },
 ];
