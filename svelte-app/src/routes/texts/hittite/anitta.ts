6dfbbbae0a8fe95d367c7968553b72c5b1ed01b6
Peter Olson <peter.e.c.olson+github@gmail.com>
2024-03-05 23:40:13 -0600

    ok done for now

--- svelte-app/src/routes/texts/hittite/anitta.ts
+++ svelte-app/src/routes/texts/hittite/anitta.ts
@@ -8,17 +8,21 @@ export const anittaText = {
     CLOUD os gen sg AT OVERCOME onts dat sg GOOD us m nom sg BE _ira sec 3 sg ,  
 	NOW AT OVERCOME onts dat sg DEICTIC TRULY GOOD us m nom sg BE _ira sec 3 sg ,  
 	URU:ne-e-sa-|as gen sg BEGET us s nom sg URU:ku-us-sa-ra-|as gen sg BEGET us s dat sg ... .  
-	BEGET us s nom sg URU:ku-us-sa-ra gen sg TOIL r yo_s abl sg NEARBY teh2 nom THICK ros m ins pl`,
+	BEGET us s nom sg URU:ku-us-sa-ra gen sg TOIL r yo_s abl sg NEARBY teh2 nom THICK ros m ins pl AWAY_GO _ira sec 3 sg ,  
+	NOW URU:ne-e-sa-|an acc sg`,
 	gloss: `Anitta son Pithāna , king Kussara , speak thus !
 	heaven at Stormgod dear was , 
 	now so Stormgod and when dear was ,
 	Nesa king Kussara king ... .
-	king Kussara city down_from`,
+	king Kussara city down_from with_multitude came ,
+	and Nesa `,
 	translation: `Anitta, Son of Pithāna, King of Kussara, speak!  
     He was dear to the Stormgod of Heaven,
 	and when he was dear to the Stormgod of Heaven,
 	the king of Nesa [verb broken off] to the king of Kussara. 
-	The king of Kussara, Pithana, came down out of the city in force, and he took the city of Nesa in the night by force.`
+	The king of Kussara, Pithana, came down out of the city in force, 
+	and he took the city of Nesa in the night by force.`
 };
 
-// LUGAL URUKu-us-sa-ra URU-az kat-ta pa-an-ga-ri-it ú-e-et nu URUNe-e-sa-an is-pa-an-di na-ak-ki-it da-a-as
+// LUGAL URUKu-us-sa-ra URU-az kat-ta pa-an-ga-ri-it ú-e-et
+// nu URUNe-e-sa-an is-pa-an-di na-ak-ki-it da-a-as
