abdeb6780cf9ea3210a4bf4d24bfee7b049e191d
Peter Olson <peter.e.c.olson+github@gmail.com>
2024-03-07 14:35:10 -0600

    meh

--- svelte-app/src/routes/texts/hittite/anitta.ts
+++ svelte-app/src/routes/texts/hittite/anitta.ts
@@ -8,12 +8,12 @@ export const anittaText = {
     CLOUD os gen sg AT OVERCOME onts dat sg GOOD us m nom sg BE _ira sec 3 sg , NOW AT OVERCOME onts dat sg DEICTIC TRULY GOOD us m nom sg BE _ira sec 3 sg , URU:ne-e-sa-|as gen sg BEGET us s nom sg URU:ku-us-sa-ra-|as gen sg BEGET us s dat sg ... .  
 	BEGET us s nom sg URU:ku-us-sa-ra gen sg TOIL r yo_s abl sg NEARBY teh2 nom THICK ros m ins pl AWAY_GO _ira sec 3 sg , NOW URU:ne-e-sa-|an acc sg NIGHTTIME o_nts dat sg ATTAIN es ins sg GIVE _ira active sec 3 sg .  
 	URU:ne-e-sa-|as gen sg BEGET us s acc sg SNATCH _ira active sec 3 sg ,  
-	BUT SON us dat pl URU:ne-e-sa-|as gen sg`,
+	BUT SON us dat pl URU:ne-e-sa-|as gen sg HATE los us n acc sg`,
 	gloss: `Anitta son of_Pithāna , king of_Kussara , speak thus !
 	heaven at Stormgod dear he_was , now so Stormgod and when dear he_was , of_Nesa king of_Kussara king ... .
 	king of_Kussara city down_from with_multitude he_came , and Nesa night with_force he_took . 
 	of_Nesa king he_captured ,
-	but to_the_children of_Nesa `,
+	but to_the_children of_Nesa evil `,
 	translation: `Anitta, Son of Pithāna, King of Kussara, speak!  
     He was dear to the Stormgod of Heaven, and when he was dear to the Stormgod of Heaven, the king of Nesa [verb broken off] to the king of Kussara. 
 	The king of Kussara, Pithana, came down out of the city in force, and he took the city of Nesa in the night by force.  
