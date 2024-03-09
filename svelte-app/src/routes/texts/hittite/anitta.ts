76ff16f9b20e7eb81c4f1d69d080ce79418e2f8a
Peter Olson <peter.e.c.olson+github@gmail.com>
2024-03-08 23:14:27 -0600

    another line

--- svelte-app/src/routes/texts/hittite/anitta.ts
+++ svelte-app/src/routes/texts/hittite/anitta.ts
@@ -11,7 +11,8 @@ export const anittaText = {
 	NOW URU:ne-e-sa-|an acc sg NIGHTTIME o_nts dat sg ATTAIN es ins sg GIVE _ira active sec 3 sg .  
 	URU:ne-e-sa-|as gen sg BEGET us s acc sg SNATCH _ira active sec 3 sg ,  
 	BUT SON us dat pl URU:ne-e-sa-|as gen sg HATE los us n acc sg NOT_THIS WHAT_DEICTIC dat sg CREATE _ira active sec 3 sg ;  
-	MOTHER acc pl DAD acc pl WORK _ira active sec 3 sg .`,
+	MOTHER acc pl DAD acc pl WORK _ira active sec 3 sg .  
+	NOW M:pi-it-ha-a-na-|as gen sg DAD gen sg I gen sg AFTER acc SINGLE m loc AND YEAR os loc POWERFUL nos f sg acc POWERFUL _ira sec 1 sg .`,
 	gloss: `Anitta son of_Pithāna , king of_Kussara , speak thus !
 	heaven at Stormgod dear he_was , 
 	now so Stormgod and when dear he_was , of_Nesa king of_Kussara king ... .
@@ -19,7 +20,8 @@ export const anittaText = {
 	and Nesa night with_force he_took . 
 	of_Nesa king he_captured ,
 	but to_the_children of_Nesa evil not to_anyone did ; 
-	mothers fathers he_made .`,
+	mothers fathers he_made .
+	and Pithāna father my after in_the_same and year revolt I_defeated .`,
 	translation: `Anitta, Son of Pithāna, King of Kussara, speak!  
     He was dear to the Stormgod of Heaven,  
 	and when he was dear to the Stormgod of Heaven, the king of Nesa [verb broken off] to the king of Kussara. 
@@ -27,5 +29,9 @@ export const anittaText = {
 	and he took the city of Nesa in the night by force.  
 	He took the King of Nesa captive,
 	but he did not do any evil to the inhabitants of Nesa;
-	instead, he treated them as mothers and fathers.`
+	instead, he treated them as mothers and fathers.
+	After my father, Pithana, I suppresed a revolt in the same year. 
+	Whatever lands rose up in the direction of the sunrise, I defeated each of the aforementioned.`
 };
+
+// nu MPi-it-ha-a-na-as at-ta-as-ma-as a-ap-pa-an sa-ni-ya ú-et-ti hu-ul-la-an-za-an hu-ul-la-nu-un
