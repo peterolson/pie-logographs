d26ec5f76bb104b32b60f5a93a154417d179f8dd
Peter Olson <peter.e.c.olson+github@gmail.com>
2024-03-12 23:48:33 -0500

    potential file

---
+++ svelte-app/src/routes/texts/chinese/kimIlSung.ts
@@ -0,0 +1,15 @@
+// 白頭山頂正日峰
+// 小白水河碧溪流
+// 光明星誕五十週
+// 皆讚文武忠孝備
+// 萬民稱頌齊同心
+// 歡呼聲高震天地
+
+// On the peak of Mt. Paekdu lies Jong-il Peak;
+// In the Sobaeksu River, the blue creek flows.
+// It is fifty years since the radiant star was born;
+// All extol his civility and martiality, his possession of loyalty and filiality.
+// The myriad people utter praise with united mind,
+// And the cries of hurrah are so loud they shake heaven and earth.
+
+// https://www.reddit.com/r/classicalchinese/comments/poj8ne/poem_by_kim_ilsung_for_kim_jongil_published_in/
