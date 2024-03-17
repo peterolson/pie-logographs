1df28d77c25efccb8ab0abf6f8b506f8d8744c69
Peter Olson <peter.e.c.olson+github@gmail.com>
2024-03-16 22:24:03 -0500

    combine lines

--- svelte-app/src/routes/texts/hittite/anitta.ts
+++ svelte-app/src/routes/texts/hittite/anitta.ts
@@ -29,12 +29,9 @@ export const anittaText = {
 	link: 'https://lrc.la.utexas.edu/eieol/hitol/10',
 	lang: 'Hittite',
 	text: `ANITTA , SON _us nom PITHANA , BEGET_KING gen sg KUSSARA , SPEAK imp 2 sg DEICTIC !  
-    CLOUD _os gen sg AT STORMGOD loc sg GOOD _us m nom sg BE sec 3 sg ,  
-	NOW AT STORMGOD loc sg DEICTIC TRULY GOOD _us m nom sg BE sec 3 sg , NESA gen sg BEGET_KING nom sg KUSSARA gen sg BEGET_KING loc sg ... .  
-	BEGET_KING nom sg KUSSARA TOIL _ryo_s abl sg NEARBY f nom THICK _ros m ins pl AWAY_GO sec 3 sg ,  
-	NOW NESA acc sg NIGHTTIME _onts loc sg ATTAIN _es ins sg GIVE active sec 3 sg .  
-	NESA gen sg BEGET_KING acc sg SNAAATCH active sec 3 sg ,  
-	BUT SON _us loc pl NESA gen sg HATE _losus n acc sg NOT_THIS WHAT_DEICTIC loc sg CREATE active sec 3 sg ; MOTHER acc pl DAD acc pl WORK active sec 3 sg .  
+    CLOUD _os gen sg AT STORMGOD loc sg GOOD _us m nom sg BE sec 3 sg , NOW AT STORMGOD loc sg DEICTIC TRULY GOOD _us m nom sg BE sec 3 sg , NESA gen sg BEGET_KING nom sg KUSSARA gen sg BEGET_KING loc sg ... .  
+	BEGET_KING nom sg KUSSARA TOIL _ryo_s abl sg NEARBY f nom THICK _ros m ins pl AWAY_GO sec 3 sg , NOW NESA acc sg NIGHTTIME _onts loc sg ATTAIN _es ins sg GIVE active sec 3 sg .  
+	NESA gen sg BEGET_KING acc sg SNAAATCH active sec 3 sg , BUT SON _us loc pl NESA gen sg HATE _losus n acc sg NOT_THIS WHAT_DEICTIC loc sg CREATE active sec 3 sg ; MOTHER acc pl DAD acc pl WORK active sec 3 sg .  
 	NOW PITHANA gen sg DAD gen sg I gen sg AFTER acc SINGLE m loc AND YEAR _os loc POWERFUL _nos f sg acc POWERFUL sec 1 sg .  
 	DAWN _teros m sg abl GROUND n sg nom WHAT n acc sg WHAT n acc sg BABBLE RAISE 3 sg sec NOW ENCLITIC_PRONOUN m pl acc ENTIRE pl acc BABBLE POWERFUL sec 1 sg .  
 	GROW _us ins , UHNAS m nom sg , BEGET_KING ZALPUWA gen sg , ZEUS acc sg ENCLITIC_1PL acc pl NESA abl sg ZALPUWA dat sg SNATCH_DO active sec 3 sg .  
@@ -46,12 +43,9 @@ export const anittaText = {
 	THIS ENCLITIC_PRONOUN m acc sg NIGHTTIME _onts loc sg ATTAIN _es ins sg GIVE active sec 1 sg ,
 	`,
 	gloss: `Anitta , son of_Pithāna , king of_Kussara , speak thus !
-	heaven at Stormgod dear he_was , 
-	now so Stormgod and when dear he_was , of_Nesa king of_Kussara king ... .
-	king of_Kussara city down_from with_multitude he_came , 
-	and Nesa night with_force he_took . 
-	of_Nesa king he_captured ,
-	but to_the_children of_Nesa evil not to_anyone did ; mothers fathers he_made .
+	heaven at Stormgod dear he_was , now so Stormgod and when dear he_was , of_Nesa king of_Kussara king ... .
+	king of_Kussara city down_from with_multitude he_came , and Nesa night with_force he_took . 
+	of_Nesa king he_captured , but to_the_children of_Nesa evil not to_anyone did ; mothers fathers he_made .
 	and Pithāna father my after in_the_same and year revolt I_defeated .
 	from_east land whatever whatever [emphatic] arose and they all [emphatic] I_defeated .
 	previously , Uhna , king of_Zalpuwa , Sius our from_Nesa to_Zalpuwa he_took .  
@@ -61,12 +55,9 @@ export const anittaText = {
 	when it after but was_suffering_famine , but it Halmasuitt god my forth gave .
 	but it in_the_night with_force I_took ,`,
 	translation: `Anitta, Son of Pithāna, King of Kussara, speak!  
-    He was dear to the Stormgod of Heaven,  
-	and when he was dear to the Stormgod of Heaven, the king of Nesa [verb broken off] to the king of Kussara. 
-	The king of Kussara, Pithana, came down out of the city in force, 
-	and he took the city of Nesa in the night by force.  
-	He took the King of Nesa captive,
-	but he did not do any evil to the inhabitants of Nesa; instead, he treated them as mothers and fathers.
+    He was dear to the Stormgod of Heaven, and when he was dear to the Stormgod of Heaven, the king of Nesa [verb broken off] to the king of Kussara. 
+	The king of Kussara, Pithana, came down out of the city in force, and he took the city of Nesa in the night by force.  
+	He took the King of Nesa captive, but he did not do any evil to the inhabitants of Nesa; instead, he treated them as mothers and fathers.
 	After my father, Pithana, I suppresed a revolt in the same year. 
 	Whatever lands rose up in the direction of the sunrise, I defeated each of the aforementioned.
 	Previously, Uhna, the king of Zalpuwas, had removed our Sius from the city of Nesa to the city of Zalpuwas.
