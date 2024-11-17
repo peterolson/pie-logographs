fa7bce682fd46c5a51d37ac4dd55258c7dc9fc4b
Peter Olson <peter.e.c.olson+github@gmail.com>
2024-11-17 00:40:53 -0600

    better gui

--- svelte-app/src/routes/char/+page.svelte
+++ svelte-app/src/routes/char/+page.svelte
@@ -1,42 +1,124 @@
 <script lang="ts">
-	const SIZE = 7;
-	const letters = [...'abcdefghijklmnopqrstuvwxyABCDEFGHIJKLMNOPQRSTUVWX'].map((letter, i) => ({
-		letter,
-		x: (i % SIZE) * 10 + SIZE,
-		y: Math.floor(i / SIZE) * 10 + SIZE
-	}));
+	import SvgChar from './SVGChar.svelte';
+
+	const SIZE = 9;
 
 	let pathCode = '';
 	let words: string[] = [];
 
-	function convertCodeToPath(code: string) {
-		code = ' ' + code;
-		let output = [];
-		for (let i = 0; i < code.length; i++) {
-			const char = code[i];
-			const prevChar = code[i - 1];
-			const letter = letters.find((l) => l.letter === char);
-			if (prevChar === ' ' && letter) {
-				output.push(`M ${letter.x} ${letter.y}`);
-			} else if (char === '-') {
-				// smooth curve
-				if (i + 2 >= code.length) {
-					break;
-				}
-				const nextChar = code[i + 1];
-				const nextLetter = letters.find((l) => l.letter === nextChar);
-				if (!nextLetter) break;
-				const nextNextChar = code[i + 2];
-				const nextNextLetter = letters.find((l) => l.letter === nextNextChar);
-				if (!nextNextLetter) break;
-				output.push(`S ${nextLetter.x} ${nextLetter.y} ${nextNextLetter.x} ${nextNextLetter.y}`);
-				i += 2;
-			} else if (letter) {
-				output.push(`L ${letter.x} ${letter.y}`);
+	type Stroke =
+		| {
+				type: 'move';
+				x: number;
+				y: number;
+		  }
+		| {
+				type: 'line';
+				x: number;
+				y: number;
+		  }
+		| {
+				type: 'quadratic';
+				x: number;
+				y: number;
+				cx: number;
+				cy: number;
+		  };
+
+	let strokes: Stroke[] = [];
+	let selectedStroke: Stroke | null = null;
+
+	function convertStrokesToSVGPath(strokes: Stroke[]) {
+		const output = [];
+		for (const stroke of strokes) {
+			const x = stroke.x * 10 + 5;
+			const y = stroke.y * 10 + 5;
+			if (stroke.type === 'move') {
+				output.push(`M ${x} ${y}`);
+			} else if (stroke.type === 'line') {
+				output.push(`L ${x} ${y}`);
+			} else if (stroke.type === 'quadratic') {
+				const cx = stroke.cx * 10 + 5;
+				const cy = stroke.cy * 10 + 5;
+				output.push(`Q ${cx} ${cy} ${x} ${y}`);
 			}
 		}
 		return output.join(' ');
 	}
+
+	function toDigit(char: string) {
+		if (!/^[1-9]$/.test(char)) {
+			return null;
+		}
+		return (+char - 1) * 10 + 5;
+	}
+
+	function convertCodeToPath(code: string) {
+		code = ' ' + code;
+		let output = [];
+		for (let i = 1; i < code.length; i += 2) {
+			const prevChar = code[i - 1];
+			let x = code[i];
+			let y = code[i + 1];
+			const areCoords = /[1-9]/.test(x) && /[1-9]/.test(y);
+			if (prevChar === ' ' && areCoords) {
+				output.push(`M ${toDigit(x)} ${toDigit(y)}`);
+			} else if (x === '-') {
+				// 	// quadratic curve
+				// 	if (i + 2 >= code.length) {
+				// 		break;
+				// 	}
+				// 	const nextChar = code[i + 1];
+				// 	const nextLetter = letters.find((l) => l.letter === nextChar);
+				// 	if (!nextLetter) break;
+				// 	const nextNextChar = code[i + 2];
+				// 	const nextNextLetter = letters.find((l) => l.letter === nextNextChar);
+				// 	if (!nextNextLetter) break;
+				// 	output.push(`Q ${nextLetter.x} ${nextLetter.y} ${nextNextLetter.x} ${nextNextLetter.y}`);
+				// 	x = nextNextLetter.x;
+				// 	y = nextNextLetter.y;
+				// 	i += 2;
+				// } else if (x === '*') {
+				// 	// circle around center
+				// 	if (i + 1 >= code.length) {
+				// 		break;
+				// 	}
+				// 	const nextChar = code[i + 1];
+				// 	if (!nextChar) break;
+				// 	const radius = +nextChar * 10;
+				// 	output.push(`M ${x + radius} ${y}`);
+				// 	output.push(`A ${radius} ${radius} 0 1 1 ${x - radius} ${y}`);
+				// 	output.push(`A ${radius} ${radius} 0 1 1 ${x + radius} ${y}`);
+				// 	i += 2;
+			} else if (areCoords) {
+				output.push(`L ${toDigit(x)} ${toDigit(y)}`);
+			} else {
+				i -= 1;
+			}
+		}
+		return output.join(' ');
+	}
+
+	const clickPoint = (x: number, y: number, isRight: boolean) => () => {
+		if (!selectedStroke) {
+			strokes.push({ type: 'move', x, y });
+		} else if (isRight && selectedStroke) {
+			if (selectedStroke.type === 'line' || selectedStroke.type === 'quadratic') {
+				const newStroke = { ...selectedStroke, type: 'quadratic', cx: x, cy: y } as Stroke;
+				strokes = strokes.map((s) => (s === selectedStroke ? newStroke : s));
+				selectedStroke = newStroke;
+				return;
+			}
+		} else {
+			strokes.push({ type: 'line', x, y });
+		}
+		strokes = [...strokes];
+		if (strokes.length) {
+			selectedStroke = strokes[strokes.length - 1];
+		}
+	};
+
+	const MOVE = 'move' as const;
 </script>
 
 <form
@@ -59,26 +141,44 @@
 
 <div>
 	<svg viewBox={`0 0 ${SIZE * 10} ${SIZE * 10}`} style="width: 400px;">
-		<path
-			d={convertCodeToPath(pathCode)}
-			fill="transparent"
-			stroke="red"
-			stroke-width="4"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-		/>
+		{#each strokes as stroke, i}
+			{#if stroke.type !== 'move' && i > 0}
+				{@const prevStroke = { type: MOVE, x: strokes[i - 1].x, y: strokes[i - 1].y }}
+				<path
+					d={convertStrokesToSVGPath([prevStroke, stroke])}
+					fill="transparent"
+					stroke={stroke === selectedStroke ? 'blue' : 'red'}
+					stroke-width="4"
+					stroke-linecap="round"
+					stroke-linejoin="round"
+				/>
+			{/if}
+		{/each}
+
 		{#each Array.from({ length: SIZE * SIZE }, (_, i) => i) as i}
+			{@const x = i % SIZE}
+			{@const y = Math.floor(i / SIZE)}
+			{@const clickHandler = clickPoint(x, y, false)}
+			{@const rightClickHandler = clickPoint(x, y, true)}
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
 			<circle
-				cx={((i * 10) % (SIZE * 10)) + SIZE}
-				cy={Math.floor(i / SIZE) * 10 + SIZE}
-				r="1"
+				cx={x * 10 + 5}
+				cy={y * 10 + 5}
+				r="2"
 				fill="yellow"
+				on:click|stopPropagation={clickHandler}
+				on:contextmenu|stopPropagation|preventDefault={rightClickHandler}
 			/>
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<!-- svelte-ignore a11y-no-static-element-interactions -->
 			<text
-				x={((i * 10) % (SIZE * 10)) + SIZE - 1}
-				y={Math.floor(i / SIZE) * 10 + SIZE + 1}
+				x={x * 10 + 4}
+				y={y * 10 + 6}
 				font-size="3"
-				fill="black">{letters[i].letter}</text
+				fill="black"
+				on:click|stopPropagation={clickHandler}
+				on:contextmenu|stopPropagation|preventDefault={rightClickHandler}>{x + 1}{y + 1}</text
 			>
 		{/each}
 	</svg>
@@ -95,17 +195,54 @@
 			/>
 		</svg>
 	{/each}
-	<svg viewBox={`0 0 ${SIZE * 10} ${SIZE * 10}`} style="width: 50px;">
-		<path
-			d={convertCodeToPath(pathCode)}
-			fill="transparent"
-			stroke="black"
-			stroke-width="2.5"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-		/>
-	</svg>
+	<SvgChar path={convertStrokesToSVGPath(strokes)} />
+
+	<SvgChar path={convertStrokesToSVGPath(strokes)} width={25} height={50} />
+	<SvgChar path={convertStrokesToSVGPath(strokes)} width={50} height={25} />
 </div>
+<button
+	on:click={() => {
+		selectedStroke = null;
+	}}
+>
+	New path
+</button>
+<button
+	on:click={() => {
+		strokes = strokes.slice(0, strokes.length - 1);
+		selectedStroke = strokes[strokes.length - 1] || null;
+	}}
+>
+	Remove last stroke
+</button>
+<button
+	on:click={() => {
+		strokes = [];
+		selectedStroke = null;
+	}}
+>
+	Clear
+</button>
+<button
+	on:click={() => {
+		if (!selectedStroke) return;
+		const selectedIndex = strokes.indexOf(selectedStroke);
+		if (selectedIndex <= 0) return;
+		selectedStroke = strokes[selectedIndex - 1] || null;
+	}}
+>
+	&lt;
+</button>
+<button
+	on:click={() => {
+		if (!selectedStroke) return;
+		const selectedIndex = strokes.indexOf(selectedStroke);
+		if (selectedIndex < 0 || selectedIndex + 1 >= strokes.length) return;
+		selectedStroke = strokes[selectedIndex + 1] || null;
+	}}
+>
+	&gt;
+</button>
 
 <style>
 	div {
---
+++ svelte-app/src/routes/char/SVGChar.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	const SIZE = 9;
+	export let path: string;
+	export let height: number = 50;
+	export let width: number = 50;
+</script>
+
+<svg
+	viewBox={`0 0 ${SIZE * 10} ${SIZE * 10}`}
+	style={`width: ${width}px; height: ${height}px;`}
+	preserveAspectRatio="none"
+>
+	<path
+		d={path}
+		fill="transparent"
+		stroke="black"
+		stroke-width="2px"
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		vector-effect="non-scaling-stroke"
+	/>
+</svg>
