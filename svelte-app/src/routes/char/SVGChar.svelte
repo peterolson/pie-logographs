782c06d0efedcdd05fd4fdf9c11f5c0b732fed1b
Peter Olson <peter.e.c.olson+github@gmail.com>
2024-11-20 13:59:30 -0600

    make punctuation less ugly

--- svelte-app/src/routes/char/SVGChar.svelte
+++ svelte-app/src/routes/char/SVGChar.svelte
@@ -6,22 +6,28 @@
 	export let width: number = 32;
 </script>
 
-<svg
-	viewBox={`-0.5 -0.5 ${SIZE} ${SIZE}`}
-	style={`width: ${width}px; height: ${height}px;padding-left:2px;`}
-	preserveAspectRatio="none"
->
-	{#if path}
-		<path
-			d={path}
-			fill="transparent"
-			stroke="black"
-			stroke-width="2px"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			vector-effect="non-scaling-stroke"
-		/>
-	{:else}
-		<rect x="0" y="0" width={SIZE} height={SIZE} fill="red" />
-	{/if}
-</svg>
+{#if path && '，。！？；：（）…'.includes(path)}
+	<span style={`font-size: ${height}px`}>
+		{path}
+	</span>
+{:else}
+	<svg
+		viewBox={`-0.5 -0.5 ${SIZE} ${SIZE}`}
+		style={`width: ${width}px; height: ${height}px;padding-left:2px;`}
+		preserveAspectRatio="none"
+	>
+		{#if path}
+			<path
+				d={path}
+				fill="transparent"
+				stroke="black"
+				stroke-width="2px"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				vector-effect="non-scaling-stroke"
+			/>
+		{:else}
+			<rect x="0" y="0" width={SIZE} height={SIZE} fill="red" />
+		{/if}
+	</svg>
+{/if}
